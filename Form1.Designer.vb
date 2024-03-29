06cd6642c275afaee2eb97ac333f412def7ee72a
Unknown X <Unknown X@DESKTOP-LM5B23R>
2024-03-28 23:55:55 -0700

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,107 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TextBox1 = New TextBox()
+        Label1 = New Label()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Font = New Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        TextBox1.Location = New Point(212, 108)
+        TextBox1.Margin = New Padding(3, 2, 3, 2)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(240, 35)
+        TextBox1.TabIndex = 0
+        TextBox1.Text = "127.0.0.0"
+        TextBox1.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("MV Boli", 15.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(212, 76)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(126, 28)
+        Label1.TabIndex = 1
+        Label1.Text = "IP Address"
+        ' 
+        ' Button1
+        ' 
+        Button1.Font = New Font("Times New Roman", 15.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button1.Location = New Point(212, 207)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(117, 53)
+        Button1.TabIndex = 2
+        Button1.Text = "Usb"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Font = New Font("Times New Roman", 15.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button2.Location = New Point(335, 207)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(117, 53)
+        Button2.TabIndex = 3
+        Button2.Text = "Wifi"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Font = New Font("Times New Roman", 15.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button3.Location = New Point(272, 148)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(117, 53)
+        Button3.TabIndex = 4
+        Button3.Text = "Connect"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.ActiveCaption
+        ClientSize = New Size(700, 338)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(Label1)
+        Controls.Add(TextBox1)
+        Margin = New Padding(3, 2, 3, 2)
+        Name = "Form1"
+        Text = "X Cast"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,25 @@
+﻿Imports System.Diagnostics
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim process As New Process()
+        process.StartInfo.FileName = "Scrcpy\Usb.bat"
+        process.Start()
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim process As New Process()
+        process.StartInfo.FileName = "Scrcpy\Wifi.bat"
+        process.Start()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        ' Get the ip address from the textbbox
+        Dim ipAddress As String = TextBox1.Text.Trim()
+        ' Execute the wifi batch script with the ip address as argument
+        Dim process As New Process()
+        process.StartInfo.FileName = "Scrcpy\Connect.bat"
+        process.StartInfo.Arguments = ipAddress
+        process.Start()
+    End Sub
+End Class
